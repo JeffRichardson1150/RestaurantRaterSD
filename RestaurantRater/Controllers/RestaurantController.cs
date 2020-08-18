2cb97b06105c0b23c5439bca60d2ef7f91e62568
JeffRichardson1150 <jeffrichardson573@gmail.com>
2020-08-17 23:51:02 -0400

    RestaurantRater Create View and Controller method

--- RestaurantRater/Controllers/RestaurantController.cs
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -1,4 +1,5 @@
-﻿using RestaurantRater.Models;
+﻿using Microsoft.Owin.Security.Provider;
+using RestaurantRater.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,30 @@ namespace RestaurantRater.Controllers
         public ActionResult Index()
         {
             return View(_db.Restaurants.ToList());
+        }
+
+        // GET : Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                // _db is a snapshot of the database
+                _db.Restaurants.Add(restaurant);
+                // Save changes to the live database form the snapshot _db
+                _db.SaveChanges();
+                // redirect to action Index
+                return RedirectToAction("Index");
+            }
+            // if the ModelState isn't valid, return the invalid model to us to display in the View
+            return View(restaurant);
         }
     }
 }
